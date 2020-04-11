88d82787edd4990127194841a68adb1bc00df351
amjedidiah <imunacode@gmail.com>
2020-04-11 23:58:14 +0100

    Challenge 4 Attempt 7

--- src/days.js
+++ src/days.js
@@ -1,9 +1,11 @@
 const timeInDays = (timeToElapse, periodType) => {
-  return {
+  const count = {
     days: timeToElapse,
     weeks: timeToElapse * 7,
     months: timeToElapse * 30
   }[periodType];
+
+  return count;
 };
 
 export default timeInDays;
